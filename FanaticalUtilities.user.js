beac15784319b2b8a9acfeab481d6f3a24de085c
mrmarble <4268580+MrMarble@users.noreply.github.com>
2018-08-28 23:24:34 +0200

    Use external  script to detect changes in the DOM

--- FanaticalUtilities.user.js
+++ FanaticalUtilities.user.js
@@ -1,37 +1,28 @@
 // ==UserScript==
 // @name         Fanatical Steam Cards
-// @namespace    http://tampermonkey.net/
-// @version      0.1
+// @namespace    https://github.com/MrMarble/FanaticalUtilities
+// @version      0.2
 // @description  Show if a game contains steam trading cards
 // @author       MrMarble
 // @match        https://www.fanatical.com/*/bundle/*
+// @updateURL    https://raw.githubusercontent.com/MrMarble/FanaticalUtilities/master/FanaticalUtilities.user.js
+// @downloadURL  https://raw.githubusercontent.com/MrMarble/FanaticalUtilities/master/FanaticalUtilities.user.js
 // @require      https://code.jquery.com/jquery-3.3.1.min.js
+// @require      https://raw.githubusercontent.com/MrMarble/FanaticalUtilities/master/waitForKeyElements.js
+// @resource     https://steamstore-a.akamaihd.net/public/images/v6/ico/ico_cards.png
 // ==/UserScript==
-
 (function() {
-    'use strict';
-    $.noConflict();
-    let runAgain = true;
+  'use strict';
+  $.noConflict();
 
-    document.getElementsByTagName('body')[0].onload = displayCards;
-    jQuery(document).ajaxComplete(function() {
-    console.log('DONE!');
-    });
+  function displayCards() {
+    let games = document.querySelectorAll('[href*="steam-trading-cards"]');
 
-    function displayCards(){
-        let cardIcon = "https://steamstore-a.akamaihd.net/public/images/v6/ico/ico_cards.png";
-        let games = document.querySelectorAll('[href*="steam-trading-cards"]');
-
-        for(let game of games){
-            let parent = game.closest(".p-3").previousSibling;
-            parent.style.borderRight = "3px solid gold";
-            let icon = document.createElement("span").appendChild(document.createTextNode("TEST TEST"));
-            parent.closest("svg").parentElement.appendChild(icon);
-        }
-
-        if (games == null && runAgain) {
-            runAgain = false;
-            setTimeout(displayCards, 250);
-        }
+    for (let game of games) {
+      let parent = game.closest(".p-3").previousSibling;
+      parent.style.borderRight = "3px solid gold";
     }
+  }
+
+  waitForKeyElements('[data-product-id]', displayCards);
 })();
